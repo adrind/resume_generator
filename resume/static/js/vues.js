a30295bc3777ab806054fcbf30e62e2c47683ed1
Adrienne <adrienne@codeforamerica.org>
2017-08-25 15:51:34 -0700

    create rich field type

--- resume/static/js/vues.js
+++ resume/static/js/vues.js
@@ -1,29 +1,24 @@
 Vue.component('preview-field', {
-  props: ['data', 'id', 'header', 'list'],
-  template: '<div>' +
-            '<h3 v-if="header">{{header}}</h3>' +
-            '<resume-list v-if="list" :list="list.values"></resume-list>' +
-            '<div class="data">{{data}}</div>' +
-            '</div>'
+    props: ['item', 'header'],
+    delimiters: ["[", "]"],
+    template: '#preview-item'
 });
 
-Vue.component('preview-rich-field', {
-  props: ['item'],
-  template: '<div>' +
-            '<h3 v-if="item.header">{{item.header}}</h3>' +
-            '<h5 v-if="item.dates">{{item.dates}}</h5>' +
-            '<resume-list v-if="item.list" :list="list.values"></resume-list>' +
-            '<div class="item.data">{{data}}</div>' +
-            '</div>'
-});
-
-
-Vue.component('resume-list', {
+Vue.component('simple-list-preview', {
     props: ['list'],
     template: '<div class="resume-list">' +
-        '<h4 v-if="list.header">{{list.header}}</h4>' +
-        '<h5 v-if="list.dates">{{list.dates}}</h5>' +
-        '<ul id="example-1"><li v-for="item in list">{{ item }}</li></ul>' +
+        '<ul id="example-1"><li v-for="data in list">{{ data }}</li></ul>' +
+        '</div>'
+});
+
+Vue.component('rich-list-preview', {
+    props: ['list'],
+    template: '<div class="resume-list">' +
+        '<div v-for="item in list" class="rich-list-preview">' +
+        '<h4 v-if="item.header">{{item.header}}</h4>' +
+        '<h5 v-if="item.dates">{{item.dates}}</h5>' +
+        '<ul v-if="item.values" id="example-1"><li v-for="data in item.values">{{ data }}</li></ul>' +
+        '</div>'+
         '</div>'
 });
 
@@ -93,7 +88,7 @@ var app = new Vue({
             createResumeField('name', 'Adrienne Dreyfus', 'What is your name?', {isActive: true}),
             createResumeField('address', '3099 Washington st', 'What is your address?'),
             createResumeField('city', 'San Francisco, CA', 'What is your city?'),
-            createResumeField('objective', 'To get better at work', "What's your goal? What do you want to learn on the job?", {isTextArea: true, header: 'Objective'}),
+            createResumeField('objective', 'To get better at work', "What's your goal? What do you want to learn during your next job?", {isTextArea: true, header: 'Objective'}),
             createSimpleList('skills', "What skills do you have?", {label: 'Add a skill:', header: 'Skills', list: {values: ['Cooking', 'Cleaning'], isSimpleList: true}}),
             createRichList('education', "What education do you have?", {label: 'Add an education:', header: 'Education', list: {values: [{header: 'Tufts', dates:'2009-2013', values:['Graduated with degree', 'Had fun']}]}})
         ],
