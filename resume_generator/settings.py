c5a24ad87788f600b15bde5035bde5a69a0275a5
Adrienne <adrienne@codeforamerica.org>
2017-09-15 16:18:21 -0700

    removed staticfile dirs

--- resume_generator/settings.py
+++ resume_generator/settings.py
@@ -123,8 +123,4 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
