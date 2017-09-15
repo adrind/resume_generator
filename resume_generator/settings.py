121689fd75e6ea126520f5116803037dcf7f77d7
Adrienne <adrienne@codeforamerica.org>
2017-09-15 16:17:26 -0700

    added static root -- fixed bug

--- resume_generator/settings.py
+++ resume_generator/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
